094d06c7632350990afa6cd5d6030759aec56986
Julio Graham <julioagraham@gmail.com>
2017-08-14 15:43:55 -0400

    Init commit

---
+++ src/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.css';
+
+const Card = (props) => (
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="eng">{props.eng}</div>
+            </div>
+            <div className="back">
+                <div className="esp">{props.esp}</div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Card;
